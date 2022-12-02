bbb7895df0b307f53f96a9ca02358c750b497bf9
Vishisht Rawat <vishisht01@gmail.com>
2022-12-03 01:15:58 +0530

    code cleaup

--- src/Components/Chessboard.js
+++ src/Components/Chessboard.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./Chessboard.css";
 const verticalAxis = ["1", "2", "3", "4", "5", "6", "7", "8"];
 const horizontalAxis = ["a", "b", "c", "d", "e", "f", "g", "h"];
